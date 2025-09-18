a179e843e9426c1db185d204f4ce23749412ee8d
naotho17 <naotho17@gmail.com>
2025-09-18 16:39:55 +0900

    ダミーデータ作成

--- database/migrations/2025_09_18_072743_create_products_table.php
+++ database/migrations/2025_09_18_072743_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('code');
+            $table->bigInteger('price');
+            $table->integer('tax');
             $table->timestamps();
         });
     }
