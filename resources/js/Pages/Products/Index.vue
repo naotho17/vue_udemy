a8a46880ee6f959b88cb39140843a3ee06410fd7
naotho17 <naotho17@gmail.com>
2025-09-28 00:30:50 +0900

    ページング機能を追加

--- resources/js/Pages/Products/Index.vue
+++ resources/js/Pages/Products/Index.vue
@@ -22,7 +22,8 @@ const deleteProduct = (id, name) =>{
 const search_go = () =>{
     form.get(route('products.index'))
 };
-// console.log(props.products.length);
+// console.log(props.products.data.length);
+// console.log(props.products);
 
 </script>
 
@@ -58,7 +59,7 @@ const search_go = () =>{
                             @blur="search_go"
                         />                
                     </div>
-                      <span v-if="props.products.length===0" class="m-2">該当する商品はありません。</span>                                    
+                      <span v-if="props.products.data.length===0" class="m-2">該当する商品はありません。</span>                                    
                 </div> 
                 <div v-if="successMessage" class="alert alert-success m-2">
                 {{ successMessage }}
@@ -76,7 +77,7 @@ const search_go = () =>{
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="product in products" :key="product.id">
+                            <tr v-for="product in products.data" :key="product.id">
                                 <td class="border border-gray-400 px-4 py-2 text-center">{{ product.id }}</td>
                                 <td class="border border-gray-400 px-4 py-2">{{ product.name }}</td>
                                 <td class="border border-gray-400 px-4 py-2 text-center">{{ product.code }}</td>
@@ -97,6 +98,56 @@ const search_go = () =>{
                          </tbody>
                     </table>   
                 </div>
+                <!-- Pagination -->
+                <nav class="flex items-center gap-x-1 mt-2 mb-2" aria-label="Pagination">
+                <div class="flex items-center gap-x-1">
+                    <div v-for="(link,index) in products.links">
+                        <div v-if="index==0">
+                            <Link :href="route('products.index', { page: products.current_page-1, search_str: form.search_str })"
+                            v-show="link['url']!=null"
+                            type="button" 
+                            class="min-h-[38px] min-w-[38px] flex justify-center items-center text-gray-800 hover:bg-gray-100 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10" 
+                            >
+                                <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <path d="m15 18-6-6 6-6"></path>
+                                </svg>
+                                <span>Previous</span>
+                            </Link>
+                        </div>
+                        <div v-else-if="index==products.last_page+1">
+                            <Link :href="route('products.index', { page: products.current_page+1, search_str: form.search_str })"
+                            v-show="link['url']!=null"
+                            type="button" 
+                            class="min-h-[38px] min-w-[38px] flex justify-center items-center text-gray-800 hover:bg-gray-100 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10" 
+                            >
+                                <span>Next</span>
+                                <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <path d="m9 18 6-6-6-6"></path>
+                                </svg>
+                            </Link>
+                        </div>
+                        <div v-else>
+                            <Link :href="route('products.index', { page: link['label'], search_str: form.search_str })"
+                            v-if="link['active']===true" 
+                            v-show="link['url']!=null"
+                            type="button" 
+                            class="min-h-[38px] min-w-[38px] flex justify-center items-center bg-gray-200 text-gray-800 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-300 disabled:opacity-50 disabled:pointer-events-none" 
+                            aria-current="page">
+                                <span>{{ link['label'] }}</span>
+                            </Link>
+                            <Link :href="route('products.index', { page: link['label'], search_str: form.search_str })"
+                            v-else 
+                            v-show="link['url']!=null"
+                            type="button" 
+                            class="min-h-[38px] min-w-[38px] flex justify-center items-center text-gray-800 hover:bg-gray-100 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10" 
+                            >
+                                <span>{{ link['label'] }}</span>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                </nav>
+                <!-- End Pagination -->                    
             </div>
         </div>
     </AuthenticatedLayout>
