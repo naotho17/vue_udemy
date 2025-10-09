adc38628275f16fa306e19161766fcae870bf582
naotho17 <naotho17@gmail.com>
2025-10-09 22:10:58 +0900

    顧客Customerテーブルにダミーデータを作成

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CustomerSeeder extends Seeder
 {
@@ -12,6 +13,16 @@ class CustomerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('customers')->insert([
+            [
+                'name' => 'A商店',
+            ],
+            [
+                'name' => 'B商店',
+            ],
+            [
+                'name' => 'C商店',
+            ],
+        ]);
     }
 }
